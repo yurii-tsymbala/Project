8abcf850a0ce96a1c0e520e5c94b37c301956013
Yurii <zumbala2013@gmail.com>
2024-03-05 23:28:51 +0200

    Configure VideoComponent

--- src/app/modules/home/pages/home/home-presentation/home-presentation.component.ts
+++ src/app/modules/home/pages/home/home-presentation/home-presentation.component.ts
@@ -1,19 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home-presentation',
   templateUrl: './home-presentation.component.html',
-  styleUrls: ['./home-presentation.component.scss']
+  styleUrls: ['./home-presentation.component.scss'],
 })
 export class HomePresentationComponent {
-  isPlaying: boolean = false;
+  @ViewChild('videoPlayer') videoPlayer!: ElementRef;
+  isOpened: boolean = false;
 
-  play() {
-
+  onPlay() {
+    this.isOpened = true;
   }
 
-  pause() {
-    
+  onClose() {
+    this.isOpened = false;
   }
 
+  onPause() {
+    const video: HTMLVideoElement = this.videoPlayer.nativeElement;
+    video.paused ? video.play() : video.pause();
+  }
 }
