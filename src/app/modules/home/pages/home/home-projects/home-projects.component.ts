261da20a5c00665cb47fae9fb690c0b9242c64e3
Yurii <zumbala2013@gmail.com>
2024-03-05 17:34:26 +0200

    Configure HomeProjectsComponent

--- src/app/modules/home/pages/home/home-projects/home-projects.component.ts
+++ src/app/modules/home/pages/home/home-projects/home-projects.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-projects',
   templateUrl: './home-projects.component.html',
   styleUrls: ['./home-projects.component.scss'],
 })
-export class HomeProjectsComponent {}
+export class HomeProjectsComponent {
+  projects: Project[] = this.generatePRojects();
+
+  formattedIndex(index: number): string {
+    let realIndex = index + 1;
+    return (realIndex + 1 < 10 ? '0' : '') + realIndex;
+  }
+
+  private generatePRojects(): Project[] {
+    return [
+      new Project('Старт', ['Ліквідація ДАБІ', 'Створення Сервісної служби']),
+      new Project('Березень - Травень 2020', [
+        'Перехідний період',
+        'Мораторій на інспекційний держконтроль',
+      ]),
+      new Project('Червень 2020', ['Створення ДІМ', 'Запуск нового реєстру']),
+      new Project('Вересень 2020', [
+        'Державне агентство з питань технічного регулювання у містобудуванні',
+      ]),
+      new Project('Січень 2021', ['Запровадження страхування']),
+    ];
+  }
+}
+
+class Project {
+  constructor(public date: string, public tasks: string[]) {}
+}
