ff71129dce63df013ae993a10954d05c21375c39
Yurii <zumbala2013@gmail.com>
2024-03-05 18:35:23 +0200

    Configure PresentComponent

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -8,10 +8,11 @@ import { LayoutModule } from 'src/app/shared/modules/layout/layout.module'
 import { HomeRoutingModule } from 'src/app/modules/home/home-routing.module'
 import { HomeHeroComponent } from 'src/app/modules/home/pages/home/home-hero/home-hero.component'
 import { HomeProjectsComponent } from 'src/app/modules/home/pages/home/home-projects/home-projects.component'
-import { HomeComponent } from 'src/app/modules/home/pages/home/home.component'
+import { HomeComponent } from 'src/app/modules/home/pages/home/home.component';
+import { HomePresentationComponent } from './pages/home/home-presentation/home-presentation.component'
 
 @NgModule({
-  declarations: [HomeComponent, HomeHeroComponent, HomeProjectsComponent],
+  declarations: [HomeComponent, HomeHeroComponent, HomeProjectsComponent, HomePresentationComponent],
   imports: [CommonModule, HomeRoutingModule, CoreModule, LayoutModule],
 })
 export class HomeModule {}
