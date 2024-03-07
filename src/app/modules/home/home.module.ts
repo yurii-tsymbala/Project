e815d5a3d9d81450c0e7b9552ffa0bd809d55726
Yurii <zumbala2013@gmail.com>
2024-03-07 04:48:54 +0200

    Configure validation

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -12,9 +12,10 @@ import { HomeComponent } from 'src/app/modules/home/pages/home/home.component';
 import { HomePresentationComponent } from './pages/home/home-presentation/home-presentation.component';
 import { HomeCorruptionComponent } from './pages/home/home-corruption/home-corruption.component';
 import { HomeNewsComponent } from './pages/home/home-news/home-news.component'
+import { ReactiveFormsModule, FormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [HomeComponent, HomeHeroComponent, HomeProjectsComponent, HomePresentationComponent, HomeCorruptionComponent, HomeNewsComponent],
-  imports: [CommonModule, HomeRoutingModule, CoreModule, LayoutModule],
+  imports: [CommonModule, HomeRoutingModule, CoreModule, LayoutModule, ReactiveFormsModule, FormsModule],
 })
 export class HomeModule {}
--- src/app/modules/home/pages/home/home-news/home-news.component.ts
+++ src/app/modules/home/pages/home/home-news/home-news.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HomeApiService } from '../../../services/home-api.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home-news',
   templateUrl: './home-news.component.html',
-  styleUrls: ['./home-news.component.scss']
+  styleUrls: ['./home-news.component.scss'],
 })
-export class HomeNewsComponent {
+export class HomeNewsComponent implements OnInit {
+  loginForm!: FormGroup;
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
 
+  constructor(private homeApiService: HomeApiService) {}
+
+  ngOnInit(): void {
+    this.configureForm();
+  }
+
+  onSubmit(): void {
+    const emailInput = this.loginForm.value.emailInput;
+
+    if (this.loginForm.valid) {
+      // this.homeApiService.logIn(emailInput);
+    }
+  }
+
+  private configureForm(): void {
+    this.loginForm = new FormGroup({
+      emailInput: this.emailFormControl
+    });
+  }
 }
